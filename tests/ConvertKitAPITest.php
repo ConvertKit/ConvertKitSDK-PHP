4fea065352898bc6b51059a74a4c46462b16ded4
Tim Carr <tim@n7studios.co.uk>
2023-03-13 17:28:26 +0000

    Added get_webhook() function and test

--- tests/ConvertKitAPITest.php
+++ tests/ConvertKitAPITest.php
@@ -694,7 +694,32 @@ class ConvertKitAPITest extends TestCase
     }
 
     /**
-     * Test that creating and destroying a webhook works.
+     * Test that get_webhooks() returns the expected data.
+     *
+     * @since   1.0.0
+     *
+     * @return void
+     */
+    public function testGetWebhooks()
+    {
+        // Create a webhook first.
+        $result = $this->api->create_webhook(
+            'https://webhook.site/2705fef6-34ef-4252-9c78-d511c540b58d',
+            'subscriber.subscriber_activate',
+        );
+        $ruleID = $result->rule->id;
+
+        // List webhooks, confirming the webhook created exists in the list.
+        $webhooks = $this->api->get_webhooks();
+        $this->assertIsArray($webhooks);
+
+        // Destroy the webhook.
+        $result = $this->api->destroy_webhook($ruleID);
+        $this->assertEquals($result->success, true);
+    }
+
+    /**
+     * Test that create_webhook() and destroy_webhook() works.
      *
      * We do both, so we don't end up with unnecessary webhooks remaining
      * on the ConvertKit account when running tests.
@@ -718,7 +743,7 @@ class ConvertKitAPITest extends TestCase
     }
 
     /**
-     * Test that creating and destroying a webhook works with an event parameter.
+     * Test that create_webhook() and destroy_webhook() works with an event parameter.
      *
      * We do both, so we don't end up with unnecessary webhooks remaining
      * on the ConvertKit account when running tests.
@@ -743,7 +768,7 @@ class ConvertKitAPITest extends TestCase
     }
 
     /**
-     * Test that create_webook() throws an InvalidArgumentException when an invalid
+     * Test that create_webhook() throws an InvalidArgumentException when an invalid
      * event is specified.
      *
      * @since   1.0.0
